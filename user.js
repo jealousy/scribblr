3f8b03c2cb7f16e1c1167724ebb96c2ff849052f
Haochen Li <haochenli@Haochens-MacBook-Air.local>
2011-09-07 01:12:30 -0400

    implemented create new user and user login

--- user.js
+++ user.js
@@ -1,9 +1,100 @@
 var Stream = require('./stream');
-function User(userId) {
-    this.userId = userId;
-//    this._load();
+var sys    = require('sys'),
+    sqlite = require('sqlite'),
+    _ = require('underscore');
+
+function User() {
+
 }
 
+User.prototype._create = function(userId, password, callback, args) {
+	var query = "INSERT INTO user (user_id, password) VALUES (?,?)";
+    var db = new sqlite.Database();
+    var self = this; // used for context
+    db.open("scribblr.db", function(error) {
+        if (error) {
+            console.log('Error connecting to db');
+            throw error;
+        }
+
+		//check if the user_id exists already
+		self._exist(userId, function(result){
+			if (!result){
+				//not exist, create new user
+		        db.execute(query, [userId, password], function(error) {
+		            if (error) throw error;
+					callback(true);
+		        });
+			}else{
+				callback(false);
+			}
+		});
+    });
+};
+
+User.prototype._exist = function(userId, callback, args) {
+    var query = "SELECT user_id FROM user WHERE " + 
+        "user_id = ?";
+
+    var db = new sqlite.Database();
+    var self = this; // used for context
+    db.open("scribblr.db", function(error) {
+        if (error) {
+            console.log('Error connecting to db');
+            throw error;
+        }
+
+        db.execute(query, [userId], function(error, rows) {
+            if (error) {
+                throw error;
+            } else {
+				if (rows.length == 0){
+					callback(false);
+				}else{
+					callback(true);
+				}
+            }
+        });
+    });
+};
+
+User.prototype._login = function(userId, password, callback, args) {
+    var query = "SELECT user_id, password FROM user WHERE " + 
+        "user_id = ? AND password = ?";
+
+    var db = new sqlite.Database();
+    var self = this; // used for context
+    db.open("scribblr.db", function(error) {
+        if (error) {
+            console.log('Error connecting to db');
+            throw error;
+        }
+
+        db.execute(query, [userId, password], function(error, rows) {
+            if (error) {
+                throw error;
+            } else {
+				if (rows.length == 0){
+					callback(false);
+				}else{
+					callback(true);
+				}
+            }
+        });
+    });
+};
+
+User.prototype.login = function(userId, password, callback, args){
+	this._login(userId, password, function(result){
+		callback(result);
+	});
+}
+
+User.prototype.createNewUser = function(userId, password, callback, args){
+	this._create(userId, password, function(result){
+		callback(result);
+	});
+}
 
 User.prototype.createStream = function(streamName, callback, args) {
     var newStream = new Stream(false, streamName, this.userId, function(streamObj) {
